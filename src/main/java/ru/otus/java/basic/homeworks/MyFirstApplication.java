a7f62717dd69033884e4db86b22347cd460a2817
Evgen Raznicyn <{earov59@gmail.com}>
2024-10-17 17:44:15 +0500

    init

---
+++ src/main/java/ru/otus/java/basic/homeworks/MyFirstApplication.java
@@ -0,0 +1,8 @@
+package ru.otus.java.basic.homeworks;
+
+public class MyFirstApplication {
+//    Комментарий
+public static void main(String[] args) {
+    System.out.println("Hello, World!");
+}
+}
